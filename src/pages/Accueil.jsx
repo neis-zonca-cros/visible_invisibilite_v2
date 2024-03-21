930e112c0eaae71d6666002ab575a0d627b74e96
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-03-21 12:22:28 +0100

    bus2

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -4,7 +4,7 @@ import bus from '../assets/bus.png'; // Remplacez 'path/to/your/image.jpg' par l
 export default function Accueil() {
   return (
     <section id="accueil" className='h-screen relative'>
-      <img src={bus} alt="Image d'un bus TCL" className="fixed top-5 left-1/2 -translate-x-1/2 h-32 lg:h-1/4 md:h-1/4 z-[97]" />
+      <img src={bus} alt="Image d'un bus TCL" className="fixed top-5 left-1/2 -translate-x-1/2 h-48 lg:h-1/4 md:h-1/4 z-[97]" />
       <div className="text-4xl sm:text-4xl md:text-6xl lg:text-6xl font-[agrandir] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-font z-10">
         Bienvenue à Lyon !
       </div>
